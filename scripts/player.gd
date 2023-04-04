7c6ec7a15cc03cda28e55d436294f6398fe07c89
NervzZ <julien.jordan@epfl.ch>
2023-04-04 17:51:37 +0200

    implemented player movements

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 
+var lookTarget = Vector2(0., 0.)
+var movements = Vector2(0., 0.)
+@export var moveSpeed = 300
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		lookTarget = event.global_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +15,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(delta)
+	look_at(lookTarget)
+	translate(movements * delta * moveSpeed)
+	
+	#Up-down movements
+	if Input.is_action_pressed("Move Up") and !Input.is_action_pressed("Move Down"):
+		movements.y = -1
+	elif Input.is_action_pressed("Move Down") and !Input.is_action_pressed("Move Up"):
+		movements.y = 1
+	else:
+		movements.y = 0
+		
+	#Left-right movements
+	if Input.is_action_pressed("Move Right") and !Input.is_action_pressed("Move Left"):
+		movements.x = 1
+	elif Input.is_action_pressed("Move Left") and !Input.is_action_pressed("Move Right"):
+		movements.x = -1
+	else:
+		movements.x = 0
