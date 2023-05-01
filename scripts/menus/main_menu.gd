05cca9f0a7a1431f1ed2852ec362de5eaf40f1a5
NervzZ <56892303+NervzZ@users.noreply.github.com>
2023-05-01 04:09:35 +0200

    bugfix/game (#10)

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -2,18 +2,6 @@ extends Node2D
 
 @export var GameScene : PackedScene
 
-
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-
 func _on_stat_game_button_down():
 	get_tree().change_scene_to_file("res://scenes/game.tscn") 
 	
