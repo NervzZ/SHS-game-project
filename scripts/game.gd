d4c5481d28ae4f55ef8b0934598b9c2ed150bb67
NervzZ <julien.jordan@epfl.ch>
2023-05-01 04:07:26 +0200

    fixed pause menu behavior and made game root playable again

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,25 +2,22 @@ extends Node2D
 
 @export var mainMenuScene : PackedScene
 
+var pause_menu_scene = load("res://scenes/menus/Pause_menu.tscn")
 var pause_menu : Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pause_menu = $PauseMenu
-	pass
-
+	pause_menu = pause_menu_scene.instantiate()
+	pause_menu.hide()
+	add_child(pause_menu)
 
 func resume_game():
 	get_tree().paused = false
-	
 
 func pause_game():
 	get_tree().paused = true
 	
+func _input(event):
+	if event.is_action_pressed("escape"):
+		pause_menu.openMenu()
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("escape"):
-		pause_menu.show()
-		pause_game()
-		
-		
--- scripts/menus/Pause_menu.gd
+++ scripts/menus/Pause_menu.gd
@@ -2,26 +2,33 @@ extends Node2D
 @export var MainMenu : PackedScene
 var offset = Vector2(-960, -540)
 
+@export var resume : Button
+@export var quit : Button
+
 @onready var character = ($"../Main Character")
 
 
-
-
 func _ready():
-	pass
+	resume.button_down.connect(_on_continue_button_pressed)
+	quit.button_down.connect(_on_quit_button_pressed)
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#faudra changer ça pour simplement lier le menu à la camera du joueur comme ça il y a jamais besoin
+#de update la position
+func _process(_delta):
 	position = character.position + offset
 
-
-func _on_continue_button_down():
+func openMenu():
+	show()
+	get_tree().paused = true
+	
+func _on_continue_button_pressed():
+	print("pressed")
 	hide()
 	get_tree().paused = false
 
 
-func _on_quit_button_down():
-	get_tree().paused = false
-	get_tree().change_scene_to_packed(MainMenu)
+func _on_quit_button_pressed():
+	print("pressed")
+	get_tree().quit()
