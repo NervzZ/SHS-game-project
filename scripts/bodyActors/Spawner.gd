32e074e4f5ace3c5418c445e0f7aa93716038442
NervzZ <julien.jordan@epfl.ch>
2023-05-02 09:52:58 +0200

    cleaner code and bug fix

--- scripts/bodyActors/Spawner.gd
+++ scripts/bodyActors/Spawner.gd
@@ -3,28 +3,18 @@ extends Node2D
 @onready var gm = get_node("/root/GameManager")
 
 @export var actor_to_spawn : PackedScene
-@export var spawn_instantly : bool = false
 @export var spawn_hours : int = 0
 @export var spawn_minutes : int = 0
-
-var hasSpawned = false
 
 func _init():
 	hide()
 
-func _ready():
-	if spawn_instantly or (spawn_hours == gm.clock.hours and spawn_minutes == gm.clock.minutes):
-		if !hasSpawned:
-			spawn()
-
 func _process(delta):
-	if !spawn_instantly and spawn_hours == gm.clock.hours and spawn_minutes == gm.clock.minutes:
-		if !hasSpawned:
-			spawn()
+	if spawn_hours == gm.clock.hours and spawn_minutes == gm.clock.minutes:
+		spawn()
 		
 func spawn():
 	var actor = actor_to_spawn.instantiate()
 	get_parent().add_child(actor)
 	actor.position = position
-	hasSpawned = true
 	queue_free()
