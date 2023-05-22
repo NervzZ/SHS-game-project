9954cc7526c62fc4a39713dc87915dee332e4a85
NervzZ <56892303+NervzZ@users.noreply.github.com>
2023-05-22 16:34:56 +0200

    main menu system done (#34)

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
-@export var GameScene : PackedScene
+@onready var colorRect: ColorRect = get_node("ColorRect")
+@onready var container: MarginContainer = get_node("MarginContainer")
+
+func _ready():
+	var viewportSize = get_viewport_rect().size
+	colorRect.set_size(viewportSize)
+	container.set_size(viewportSize)
+	GameManager.UI.hide()
+	GameManager.pause_game()
+	
 
 func _on_stat_game_button_down():
-	get_tree().change_scene_to_file("res://scenes/game.tscn") 
+	GameManager.UI.show()
+	GameState.resetStates()
+	GameManager.goto_scene(Levels.Levels.BEDROOM_LEVEL, 5, 30)
+	GameManager.resume_game()
 	
 
 func _on_exit_button_down():
