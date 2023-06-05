4fc72b6a6022fe3488a81d85b49eed7be46f5495
NervzZ <56892303+NervzZ@users.noreply.github.com>
2023-06-05 20:04:19 +0200

    feature/objective-system (#41)

--- scripts/Singletons/Globals.gd
+++ scripts/Singletons/Globals.gd
@@ -7,3 +7,15 @@ const gameItems := {
 	},
 	# Add more items here if needed
 }
+
+const objectives := {
+	"Go to work": {
+		"description": "I need to go to work. If I'm late again my boss is going to kill me.",
+		"nextObjective": "Enter Cyber Shield"
+	},
+	
+	"Enter Cyber Shield": {
+		"description": "Alright, time to enter in the office and get to work",
+		"nextObjective": null #entrer ici le nom de l'objectif suivant
+	}
+}
